278b551fca31e948dbed7f002d886b73a98fae94
MatheusGiovani633 <MatheusGiovani633>
2025-11-17 20:53:07 -0300

    Criado layout de login

--- app/src/main/java/com/example/plantasking/MainActivity.kt
+++ app/src/main/java/com/example/plantasking/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,11 +34,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import com.example.plantasking.ui.theme.PlantAskingTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,7 +61,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
-    var loggedIn by remember { mutableStateOf(false) }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val textFieldsColors = OutlinedTextFieldDefaults.colors(
+      focusedTextColor = Color.White,
+      unfocusedTextColor = Color.White,
+      focusedBorderColor = Color.Transparent,
+      unfocusedBorderColor = Color.Transparent,
+      focusedContainerColor = Color.Transparent,
+      unfocusedContainerColor = Color.Transparent,
+      focusedLabelColor = Color.White,
+      unfocusedLabelColor = Color.White,
+      cursorColor = Color.White
+    )
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -61,32 +85,132 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                         Color(0xFF19B27A)
                     )
                 )
-            )
+            ),
+        contentAlignment = Alignment.Center
     ) {
         Column(
-           Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Row (
-
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ){
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+            )
+            Text(
+                text = "Bem vindo ao PlantAsking",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = {
+                        Text(
+                            "Email",
+                            color = Color.White.copy(alpha = 0.7f),
+                            fontWeight = FontWeight.Bold)
+                        },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
+                colors = textFieldsColors
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(3.dp)
+                    .width(335.dp)
+                    .background(
+                        color = Color.White.copy(alpha = 0.6f),
+                        shape = RoundedCornerShape(percent = 100)
+                    )
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(
+                        "Senha",
+                        color = Color.White.copy(alpha = 0.7f),
+                        fontWeight = FontWeight.Bold)
+                },
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp),
+                colors = textFieldsColors
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(3.dp)
+                    .width(335.dp)
+                    .background(
+                        color = Color.White.copy(alpha = 0.6f),
+                        shape = RoundedCornerShape(percent = 100)
+                    )
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp)
+            )
+            Button(
+                onClick = { /* TODO */ },
+                modifier = Modifier
+                    .width(320.dp)
+                    .height(50.dp),
+                shape = RoundedCornerShape(percent = 15),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black
+                )
+            ) {
                 Text(
-                    text = "Login",
-                    color = Color.White
+                    text = "Entrar",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold
                 )
             }
-        }
-        if (loggedIn) {
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp)
+            )
+            Row {
+                Text(
+                    text = "Esqueceu a senha? ",
+                    color = Color.White,
+                    fontSize = 15.sp
+                )
+                Text(
+                    text = "Clique aqui",
+                    color = Color.Blue.copy(alpha = 0.8f),
+                    fontSize = 15.sp,
+                    modifier = Modifier.clickable {
+                        
+                    }
+                )
+            }
 
         }
     }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreen() {
+
+}
+@Preview()
+@Composable
+fun TelaInicial() {
+    var loggedIn by remember { mutableStateOf(false) }
     PlantAskingTheme {
-        LoginScreen()
+        if (loggedIn) {
+            HomeScreen()
+        }
+        else{
+            LoginScreen()
+        }
     }
 }
